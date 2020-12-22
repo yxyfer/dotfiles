c1a457ddcc43273c43263a42201c8e84e3206520
Mathieu Rivier <mathieu.rivier@epita.fr>
2020-12-22 18:18:10 +0100

    Updated install

--- files/terminal/custom/custom_messages.sh
+++ files/terminal/custom/custom_messages.sh
@@ -29,11 +29,11 @@ check_success_message()
 
 end_message()
 {
-    if [[ $STATUS -eq 0 ]]; then
-         echo "[\e[32mPASS\e[39m] $1 installed."
+    if [[ $1 -eq 0 ]]; then
+         echo "[\e[32mPASS\e[39m] $2 installed."
          return 0;
     else
-        echo "[\e[31mFAIL\e[39m] problem while installing $1."
+        echo "[\e[31mFAIL\e[39m] problem while installing $2."
         return 1;
     fi
 }
